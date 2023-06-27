666cb30efbb27a9e548b0da3dba2fb723f0aa309
zkhan1994 <zeshan.khan1994@outlook.com>
2023-06-27 19:00:41 -0400

    Units Added

--- WeatherMap.py
+++ WeatherMap.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+
 user_apid = '83d4c9d9903d03a41f1186e450c4aaf9'
 
 apiUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=30144&units=imperial&appid='+user_apid
