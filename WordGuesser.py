7e21cc556507625d8bbecb366bbd5753422f47e4
zkhan1994 <zeshan.khan1994@outlook.com>
2023-06-27 17:35:43 -0400

    Final Copy of Word Guesser

--- WordGuesser.py
+++ WordGuesser.py
@@ -1,1 +1,20 @@
-dsafsdf def word_guesser():
+def guessLetter():
+
+    letters = ["f", "o", "x"]
+    guessedLetters = []
+
+    while len(guessedLetters) < len(letters):
+        lets = input("Enter letter: ")
+
+        if lets in letters and lets not in guessedLetters:
+            guessedLetters.append(lets)
+            print(f"Congrats! You've found a new letter: {lets}")
+        else:
+            print("Incorrect or already guessed letter.")
+
+        print(f"Current guessed letters: {guessedLetters}")
+
+    print("Congrats! You won.")
+
+
+guessLetter()
